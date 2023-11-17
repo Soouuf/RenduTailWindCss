735c92724bd0fc2fd772645c53bb2b5a8f077730
Olivier Gorzalka <olivier@amphibee.fr>
2023-11-17 16:32:13 +0100

    Ajout pagination

--- src/js/components/FlightTable.js
+++ src/js/components/FlightTable.js
@@ -1,16 +1,27 @@
 export default function FlightTable() {
     return {
+        perPage: 5,
+        currentPage: 1,
         from: '',
         to: '',
+        distanceMin: 0,
+        totalPages: 0,
+        distanceMax: 10000,
         datas: [],
         baseDatas: [],
+        filteredDatas: [],
+
         init() {
             fetch('./public/data.json')
                 .then(response => response.json())
                 .then(data => {
-                    this.datas = this.baseDatas = data;
+                    this.datas = this.filteredDatas = this.baseDatas = data;
+                    this.filterTable();
                 })
                 .catch(error => console.error('Erreur de chargement:', error));
+        },
+        countTotalPages() {
+            this.totalPages = Math.ceil(this.filteredDatas.length / this.perPage);
         },
         fromValues() {
             return [...new Set(this.baseDatas.map(data => data.from))].sort();
@@ -19,11 +30,27 @@ export default function FlightTable() {
             return [...new Set(this.baseDatas.map(data => data.to))].sort();
         },
         filterTable() {
-            this.datas = this.baseDatas.filter(data => {
+            this.filteredDatas = this.baseDatas.filter(data => {
                 let fromMatches = this.from !== '' ? (data.from === this.from) : true;
                 let toMatches = this.to !== '' ? (data.to === this.to) : true;
-                return fromMatches && toMatches;
+                let distanceMinMatch = this.distanceMin >= 0 && this.distanceMin !== '' ? (data.kms >= this.distanceMin) : true;
+                let distanceMaxMatch = this.distanceMax <= 10000 && this.distanceMax !== '' ? (data.kms <= this.distanceMax) : true;
+                return fromMatches && toMatches && distanceMinMatch && distanceMaxMatch;
             });
+            this.paginateTable();
+        },
+
+        setCurrentPage(page) {
+            this.currentPage = page;
+            this.paginateTable();
+        },
+
+        paginateTable() {
+            this.countTotalPages();
+            // décalage est l'index du tableau à partir duquel commencer à prendre les éléments
+            let offset = (this.currentPage - 1) * this.perPage;
+            // nous obtenons la sous-séquence du tableau original à afficher sur la page courante
+            this.datas = this.filteredDatas.slice(offset, offset + this.perPage);
         }
     }
 }
