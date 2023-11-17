9757e553a949a09579d814637da95085bd3fe685
Olivier Gorzalka <olivier@amphibee.fr>
2023-11-17 15:45:15 +0100

    Ajout des trajets au format json + ajout filtre départ et destination

--- src/js/components/FlightTable.js
+++ src/js/components/FlightTable.js
@@ -1,8 +1,29 @@
-export default function FlightTable(datas) {
+export default function FlightTable() {
     return {
+        from: '',
+        to: '',
         datas: [],
+        baseDatas: [],
         init() {
-            this.datas = datas;
+            fetch('./public/data.json')
+                .then(response => response.json())
+                .then(data => {
+                    this.datas = this.baseDatas = data;
+                })
+                .catch(error => console.error('Erreur de chargement:', error));
+        },
+        fromValues() {
+            return [...new Set(this.baseDatas.map(data => data.from))].sort();
+        },
+        toValues() {
+            return [...new Set(this.baseDatas.map(data => data.to))].sort();
+        },
+        filterTable() {
+            this.datas = this.baseDatas.filter(data => {
+                let fromMatches = this.from !== '' ? (data.from === this.from) : true;
+                let toMatches = this.to !== '' ? (data.to === this.to) : true;
+                return fromMatches && toMatches;
+            });
         }
     }
 }
