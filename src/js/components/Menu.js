6407b5dfdcf31d280ab314a3024ef67dbedfb4cc
Olivier Gorzalka <olivier@amphibee.fr>
2023-11-24 10:14:09 +0100

    Ajout menu alpinejs

--- src/js/components/Menu.js
+++ src/js/components/Menu.js
@@ -1,83 +1,28 @@
-const comparisonOperations = {
-    'equal': (a, b) => a === b,
-    'notEqual': (a, b) => a !== b,
-    'greaterThan': (a, b) => a > b,
-    'greaterThanOrEqual': (a, b) => a >= b,
-    'lessThan': (a, b) => a < b,
-    'lessThanOrEqual': (a, b) => a <= b
-};
-
-const maybeConvertToInteger = (str) => {
-    const value = parseInt(str, 10);
-    return isNaN(value) ? str : value;
-}
-
-const setModelFilters = (that) => {
-    that.$el.querySelectorAll('[x-model^="filter."]').forEach(function (item) {
-        let modelName = item.getAttribute('x-model');
-        let key = that.getFilterKey(modelName);
-        that.filter[key] = item.value;
-        that.filters[key] = {
-            compare: item.dataset.filterCompare || 'equal',
-            keyValue: item.dataset.filterValue || ''
-        };
-    });
-}
-
-const setDatas = (that, data) => {
-    that.datas = that.filteredDatas = that.baseDatas = data;
-    that.filterTable();
-}
-
-export default function FlightTable(datas) {
+export default function Menu() {
     return {
-        currentPage: 1,
-        perPage: 5,
-        filters: [],
-        filter: [],
-        filteredDatas: [],
-        totalPages: 0,
-        distanceMax: 10000,
-        datas: [],
-        baseDatas: [],
+        currentMenuId: 0,
         init() {
-            setModelFilters(this);
-            fetch(datas)
-                .then(response => response.json())
-                .then(data => setDatas(this, data))
-                .catch(error => console.error('Erreur de chargement:', error));
+            this.assignIdsToAllSubMenus();
         },
-        countTotalPages() {
-            this.totalPages = Math.ceil(this.filteredDatas.length / this.perPage);
+        assignIdsToAllSubMenus() {
+            let subMenuId = 1;
+
+            this.$el.querySelectorAll('.sub-menu').forEach(item => {
+                item.dataset.subMenuId = subMenuId;
+                subMenuId++;
+            });
         },
-        fromValues() {
-            return [...new Set(this.baseDatas.map(data => data.from))].sort();
+        isOpen() {
+            return this.currentMenuId === this.getSubMenuIdFromElement();
         },
-        toValues() {
-            return [...new Set(this.baseDatas.map(data => data.to))].sort();
+        openMenu() {
+            this.currentMenuId = this.getCurrentSubMenuId();
         },
-        getFilterKey(modelName) {
-            return modelName.split('.')[1];
+        getSubMenuIdFromElement(){
+            return parseInt(this.$el.dataset.subMenuId) || 0;
         },
-        filterTable() {
-            this.filteredDatas = this.baseDatas.filter(data =>
-                Object.keys(this.filters).every(key => {
-                    const { keyValue, compare } = this.filters[key];
-                    const filterValue = maybeConvertToInteger(this.filter[key]) || false;
-                    const dataValue = maybeConvertToInteger(data[keyValue]);
-                    return !filterValue || comparisonOperations[compare](dataValue, filterValue);
-                })
-            );
-            this.paginateTable();
-        },
-        setCurrentPage(page) {
-            this.currentPage = page;
-            this.paginateTable();
-        },
-        paginateTable() {
-            this.countTotalPages();
-            let offset = (this.currentPage - 1) * this.perPage;
-            this.datas = this.filteredDatas.slice(offset, offset + this.perPage);
+        getCurrentSubMenuId(){
+            return parseInt(this.$el.closest('li').querySelector('.sub-menu').dataset.subMenuId);
         }
-    }
-}
+    };
+};
